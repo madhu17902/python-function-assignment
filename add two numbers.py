c2073d01597bdd6ff89166c1f742890ad163a5d8
madhu17902 <1mj22ec090@mvjce.edu.in>
2026-02-24 09:44:47 +0530

    Create add two numbers.py

---
+++ add two numbers.py
@@ -0,0 +1,8 @@
+def add_numbers(a, b):
+    result = a + b
+    return result
+
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+
+print("Sum is:", add_numbers(x, y))
